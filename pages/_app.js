c943b1f7af7c45932e584481611df50acbbfa63f
FunZd011 <domingofunez99@gmail.com>
2022-02-07 16:22:23 -0600

    new structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import 'bootswatch/dist/darkly/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
-import '../styles/global.css'
 
 const MyApp = ({Component, pageProps}) => {
     return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Header from "../components/header";
+import Whoiam from "../components/whoiam";
 
 const Home = () => {
     return (
@@ -13,65 +13,8 @@ const Home = () => {
             </Head>
             <main className={"bg-black"}>
                 <section>
-                    <Header></Header>
+                    <Whoiam></Whoiam>
                 </section>
-
-                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias dolore magnam modi obcaecati
-                    pariatur perferendis quidem quod, tempore temporibus tenetur. Consectetur exercitationem explicabo
-                    hic, maiores nulla quos tempora unde vitae.
-                </div>
-                <div>Asperiores fugit provident quaerat, quasi rerum saepe sint unde! Delectus iusto maxime non nulla
-                    tempora. A aliquid blanditiis dolor eligendi hic ipsa libero, magni modi nulla, odio quas ratione
-                    similique?
-                </div>
-                <div>Accusamus cumque doloremque est fugiat iusto labore, maxime, natus officia reiciendis sapiente,
-                    vitae voluptate. Doloremque eius itaque nam obcaecati quisquam repellat. Aliquid explicabo maiores
-                    nobis obcaecati rem similique soluta, tempora.
-                </div>
-                <div>Doloremque eius facilis laudantium nemo neque officia provident quo totam voluptas voluptatum!
-                    Culpa dignissimos eius facere ipsa magnam quidem tempore veritatis. Aliquid beatae magnam nam
-                    numquam porro quisquam repellat, velit!
-                </div>
-                <div>A assumenda ea eius est expedita, iste obcaecati perferendis sunt. Accusantium at doloremque, eaque
-                    ex explicabo facilis numquam quam qui quibusdam totam ullam veniam. Accusantium atque aut culpa
-                    fugiat hic!
-                </div>
-                <div>Adipisci atque cum debitis earum eius eos, error et fugiat illum minima molestias officia
-                    perspiciatis, sequi totam, vitae. Accusamus est excepturi ipsa numquam officia quo sequi suscipit
-                    veritatis. Error, id.
-                </div>
-                <div>Consequuntur culpa excepturi hic iste minima molestiae, necessitatibus perspiciatis provident quae
-                    tempore, voluptas voluptatibus. Alias asperiores beatae distinctio nihil obcaecati odit optio quos
-                    reiciendis repellendus voluptatum. Illum molestias qui reprehenderit.
-                </div>
-                <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus architecto asperiores autem,
-                    distinctio eos esse eveniet exercitationem laudantium necessitatibus nisi officiis provident quam
-                    quia quis repellendus sint sit tempore velit.
-                </div>
-                <div>Dicta, iste ratione! Molestias mollitia quae quod quos? Cum deserunt expedita incidunt libero
-                    molestias nam nisi, officiis praesentium velit. Minus, mollitia, tempore. Alias, atque deleniti
-                    iusto numquam reiciendis sequi sunt.
-                </div>
-                <div>Deleniti eaque exercitationem incidunt ipsum, minima odio omnis perspiciatis quas vero
-                    voluptatibus. Aspernatur commodi dolores eos iure laudantium, molestiae nisi omnis praesentium
-                    quisquam recusandae repudiandae saepe sequi soluta ullam veniam!
-                </div>
-                <div>Alias aperiam cum dolores fuga fugiat harum hic impedit ipsa magni minus nisi non obcaecati officia
-                    officiis, omnis pariatur quas quis quod reprehenderit sapiente sed similique sunt temporibus
-                    veritatis voluptatem?
-                </div>
-                <div>Ad culpa dolore exercitationem nihil omnis placeat repellat similique velit. Blanditiis consequatur
-                    dolores eius facere, fugiat itaque iusto labore laboriosam laborum, nostrum obcaecati qui sequi
-                    velit vero voluptatum. Facere, neque?
-                </div>
-                <div>Aliquam consequatur dolorum illo illum impedit maiores nihil recusandae repudiandae voluptate.
-                    Architecto, cum deleniti dignissimos distinctio eligendi incidunt laudantium modi nobis odit placeat
-                    quam quasi tempora veritatis vero voluptatum? Iusto?
-                </div>
-                <div>Atque ipsam officia quas? Consequuntur necessitatibus nobis perspiciatis porro quis repudiandae!
-                    Cum error explicabo illo illum impedit, ipsa nesciunt placeat quasi velit vitae. Aut dolorum ipsa
-                    ipsam optio, possimus praesentium.
-                </div>
             </main>
         </div>
     )
