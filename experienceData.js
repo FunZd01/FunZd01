dab3e44de3027a7b377ec03d93fdfb3ce076c470
funzd01 <domingofunez99@gmail.com>
2022-04-01 11:45:49 -0600

    experiences data

---
+++ experienceData.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        'title': 'Python backend developer',
+        'description': 'ahorita descriptivo nada mas, luego especificar',
+        'img': "https://res.cloudinary.com/dzgnrldzj/image/upload/v1648834904/portfolio/python_sbv7yw.png"
+    },
+    {
+        'title': 'Python backend developer',
+        'description': 'ahorita descriptivo nada mas, luego especificar',
+        'img': "https://res.cloudinary.com/dzgnrldzj/image/upload/v1648834904/portfolio/python_sbv7yw.png"
+    },
+    {
+        'title': 'Python backend developer',
+        'description': 'ahorita descriptivo nada mas, luego especificar',
+        'img': "https://res.cloudinary.com/dzgnrldzj/image/upload/v1648834904/portfolio/python_sbv7yw.png"
+    },
+]
